92443fb3732d7fe7a0317468bad9fb4814521d95
mitramitro <mitramitro@gmail.com>
2021-02-27 15:38:52 +0700

    halaman login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+ 
 
 class CreateUsersTable extends Migration
 {
@@ -22,6 +25,12 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+    
+        User::create([
+            'name' => 'Mitro',
+            'email' => 'mitramitro@gmail.com',
+            'password' => Hash::make('123')
+        ]);
     }
 
     /**
